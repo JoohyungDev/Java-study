bbde33fe99a9d267a205f1d3c8e8eb1bb87b929e
JooHyung <jhp21c@naver.com>
2025-02-02 15:32:52 +0900

    Simple equals() vs. IDE-created equals()

--- java-mid1/src/lang/Object/equals/UserV2.java
+++ java-mid1/src/lang/Object/equals/UserV2.java
@@ -1,5 +1,7 @@
 package lang.Object.equals;
 
+import java.util.Objects;
+
 public class UserV2 {
 
     private String id;
@@ -8,9 +10,17 @@ public class UserV2 {
         this.id = id;
     }
 
-    @Override
+/*    @Override
     public boolean equals(Object obj) {
         UserV2 user = (UserV2) obj;
         return id.equals(user.id);
+    }
+*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserV2 userV2 = (UserV2) o;
+        return Objects.equals(id, userV2.id);
     }
 }
