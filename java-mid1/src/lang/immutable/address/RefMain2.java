503e5cc9b31cdd7f7b701bc87a2a9986c6c80990
JooHyung <jhp21c@naver.com>
2025-02-04 13:58:21 +0900

    Immutable object examples

---
+++ java-mid1/src/lang/immutable/address/RefMain2.java
@@ -0,0 +1,18 @@
+package lang.immutable.address;
+
+public class RefMain2 {
+
+    public static void main(String[] args) {
+        //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
+        Address a = new Address("서울");
+        Address b = a; //참조값 대입을 막을 수 있는 방법이 없다.
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        
+        //b.setValue("부산"); //b의 값을 부산으로 변경해야함
+        b = new Address("부산");
+        System.out.println("부산 -> b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
